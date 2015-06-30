9d9dbbd873ec415d1a8d443803bad52ed2d3058b
Bruno La Pastina <brunolap@gmail.com>
2015-06-30 17:04:08 -0300

    Added others register map
    Added map of the PWM, PCM and part of the clock management registers

--- iopin/iopin.h
+++ iopin/iopin.h
@@ -8,6 +8,7 @@ struct SIOPinDev
    ulong ulPin;
 };
 
+//---[ GPIO Registers map ]---
 struct SGpioRegistersMap
 {
    uint32_t GPFSEL[6];     // [R/W] GPIO Function Select
@@ -34,6 +35,54 @@ struct SGpioRegistersMap
    uint32_t Reserved11;
    uint32_t GPPUD;         // [R/W] GPIO Pin Pull-up/down Enable
    uint32_t GPPUDCLK[2];   // [R/W] GPIO Pin Pull-up/down Enable Clock
+};
+
+//---[ PWM Registers map ]---
+struct SPWMRegistersMap
+{
+   uint32_t CTL;           // PWM Control
+   uint32_t STA;           // PWM Status
+   uint32_t DMAC;          // PWM DMA Configuration
+   uint32_t Reserved1;
+   uint32_t RNG1;          // PWM Channel 1 Range
+   uint32_t DAT1;          // PWM Channel 1 Data
+   uint32_t FIF1;          // PWM FIFO Input
+   uint32_t Reserved2;
+   uint32_t RNG2;          // PWM Channel 2 Range
+   uint32_t DAT2;          // PWM Channel 2 Data
+};
+
+//---[ PCM Registers map ]---
+struct SPWMRegistersMap
+{
+   uint32_t CS_A;          // PCM Control and Status
+   uint32_t FIFO_A;        // PCM FIFO Data
+   uint32_t MODE_A;        // PCM Mode
+   uint32_t RXC_A;         // PCM Receive Configuration
+   uint32_t TXC_A;         // PCM Transmit Configuration
+   uint32_t DREQ_A;        // PCM DMA Request Level
+   uint32_t INTEN_A;       // PCM Interrupt Enables
+   uint32_t INTSTC_A;      // PCM Interrupt Status & Clear
+   uint32_t GRAY;          // PCM Gray Mode Control
+};
+
+//---[ Clock Registers map ]---
+// The clock map on the datasheet is not complete. It has only the GPIO clock.
+// I menage to find the PWM clock too, but anything is going to need some more research
+struct SPWMRegistersMap
+{
+   uint32_t Unknown1[28]   // Off=0x00 to 0x70 - 28 Unkown registers
+   uint32_t CM_GP0CTL;     // Off=0x70 - Clock Manager General Purpose Clocks Control 0
+   uint32_t CM_GP0DIV;     // Off=0x74 - Clock Manager General Purpose Clock Divisors 0
+   uint32_t CM_GP1CTL;     // Off=0x78 - Clock Manager General Purpose Clocks Control 1
+   uint32_t CM_GP1DIV;     // Off=0x7C - Clock Manager General Purpose Clock Divisors 1
+   uint32_t CM_GP2CTL;     // Off=0x80 - Clock Manager General Purpose Clocks Control 2
+   uint32_t CM_GP2DIV;     // Off=0x84 - Clock Manager General Purpose Clock Divisors 2
+   uint32_t Unknown2[4];   // Off=0x88 to 0x9C - 4 Unkown registers
+   uint32_t CM_PCMCTL;     // Off=0x98 - Clock Manager PCM Clock Control
+   uint32_t CM_PCMDIV;     // Off=0x9C - Clock Manager PCM Clock Divisors
+   uint32_t CM_PWMCTL;     // Off=0xA0 - Clock Manager PWM Clock Control
+   uint32_t CM_PWMDIV;     // Off=0xA4 - Clock Manager PWM Clock Divisors
 };
 
 static int ContructDevice( struct SIOPinDev* pobjDev, int iMinor, struct class* pobjClass );
